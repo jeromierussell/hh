deccf6b045d17271667497338f19c492ed97c509
jeromierussell <jeromierussell@gmail.com>
2016-04-23 08:33:11 -0400

    2016

--- hh.rb
+++ hh.rb
@@ -9,7 +9,7 @@ end
 
 def do_stuff
 	# the_time = Time.local(2013, 5, 25, 6)
-	the_time = Time.local(2016, 5, 8, 6)
+	the_time = Time.local(2016, 5, 28, 6)
 
 	(the_time - Time.now).to_i / (24 * 60 * 60)
 end
